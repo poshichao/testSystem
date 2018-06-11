341a0d0250b18640c7fe8bb05b61f454ff8335f3
chaoweb <1648907448@qq.com>
2018-06-11 19:42:45 +0800

    登录

--- public/static/assets/js/scripts.js
+++ public/static/assets/js/scripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
+    $.backstretch("/testSystem/public/static/assets/img/backgrounds/1.jpg");
     
     /*
         Form validation
