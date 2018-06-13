58b1a7eb0a3377dcbd251245af7d57e81612f884
chaoweb <1648907448@qq.com>
2018-06-13 21:33:32 +0800

    知识点添加

--- application/index/controller/TopicController.php
+++ application/index/controller/TopicController.php
@@ -5,13 +5,12 @@
  * Date: 2018/6/12
  * Time: 19:14
  */
-
 namespace app\index\controller;
-
 
 use app\index\model\Topic;
 use think\Controller;
 use app\index\controller\IndexController;
+use think\Request;
 
 class TopicController extends IndexController
 {
@@ -24,7 +23,21 @@ class TopicController extends IndexController
     }
 
     public function add() {
+        return $this->fetch();
+    }
 
+    public function insert() {
+        $post = Request::instance()->post();
+
+        $Topic = new Topic();
+        $Topic->name = $post['name'];
+        $Topic->course_id = $post['course_id'];
+
+        if ($Topic->save()) {
+            return $this->success('保存成功！', url('index'));
+        } else {
+            return $this->error('保存失败！');
+        }
     }
 
     public function edit() {
