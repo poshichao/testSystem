0b963213e0981a43dfa4d51c3d017910f53ad40c
chaoweb <1648907448@qq.com>
2018-06-13 09:10:50 +0800

    教师编辑，删除

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -38,10 +38,47 @@ class TeacherController extends IndexController {
 	}
 
 	public function edit() {
+		$teacherId = Request::instance()->param('id/d');
+
+		if (is_null($Teacher = Teacher::get($teacherId))) {
+			return $this->error('未找到id为' . $teacherId . '的教师！');
+		}
+
+		$this->assign('Teacher', $Teacher);
+		return $this->fetch();
 
 	}
 
+	public function update() {
+		$post = Request::instance()->post();
+		$teacherId = Request::instance()->param('id/d');
+
+		$Teacher = Teacher::get($teacherId);
+		$Teacher->name = $post['name'];
+		$Teacher->sex = $post['sex'];
+		$Teacher->password = $post['password'];
+		$Teacher->work_number = $post['work_number'];
+		$Teacher->klass_id = $post['klass_id'];
+
+		if ($Teacher->validate()->save()) {
+			return $this->success('保存成功！', url('index'));
+		} else {
+			return $this->error('保存失败！');
+		}
+	}
+
 	public function delete() {
-		
+		$teacherId = Request::instance()->param('id/d');
+
+		$Teacher = Teacher::get($teacherId);
+		if (!is_null($Teacher)) {
+			if ($Teacher->delete()) {
+				return $this->success('删除成功！', url('index'));
+			} else {
+				return $this->error('删除失败！');
+			}
+		} else {
+			return $this->error('未找到id为' . $teacherId . '的对象');
+		}
 	}
 }
