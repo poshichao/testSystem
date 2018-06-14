9cd22e9cac985474ebc725b5dcc3d9003fd1d035
chaoweb <1648907448@qq.com>
2018-06-14 14:56:28 +0800

    题目编辑

--- application/index/controller/ItemController.php
+++ application/index/controller/ItemController.php
@@ -43,7 +43,30 @@ class ItemController extends IndexController
     }
 
     public function edit() {
+        $itemId = Request::instance()->param('id/d');
 
+        $item = Item::get($itemId);
+        $this->assign('item', $item);
+        return $this->fetch();
+    }
+
+    public function update() {
+        $itemId = Request::instance()->param('id/d');
+        $post = Request::instance()->post();
+
+        $Item = Item::get($itemId);
+
+        if (is_null($Item)) {
+            return $this->error('未找到id为' . $itemId . '的对象');
+        }
+        $Item->dry = $post['dry'];
+        $Item->option = $post['option'];
+
+        if ($Item->save()) {
+            return $this->success('保存成功！', url('index'));
+        } else {
+            return $this->error('保存失败！');
+        }
     }
 
     public function delete() {
