a11c33599df9ddff91e399fdac28d0c364e24082
胡启明 <476008081@qq.com>
2018-06-20 08:21:38 +0800

    考试界面倒计时

--- application/index/controller/PaperController.php
+++ application/index/controller/PaperController.php
@@ -19,8 +19,8 @@ class PaperController extends IndexController
     {
         $paperId = Request::instance()->param('id');
         $paper = Paper::getPaperWithItems($paperId);
-        $this->assign('Paper',$paper);
-        $this->assign('items',$paper->items);
+        $this->assign('Paper', $paper);
+        $this->assign('items', $paper->items);
         return $this->fetch();
     }
 
@@ -30,19 +30,40 @@ class PaperController extends IndexController
         $paper = Paper::getPaperWithItems($paperId);
         $offset = $paper->time * 60;
         $ept = self::setExpectTime($offset);
-        $this->assign('ept',$ept);
-        $this->assign('Paper',$paper);
-        $this->assign('items',$paper->items);
+        $this->assign('ept', $ept);
+        $this->assign('Paper', $paper);
+        $this->assign('items', $paper->items);
         return $this->fetch();
     }
 
-    private function setExpectTime($offset){
+    private function setExpectTime($offset)
+    {
         $paperId = Request::instance()->param('id');
         $paper = Paper::get($paperId);
-        $score = (new Score())->getScoreByExamID($paper->exam_id,session('studentId'));
+        $score = (new Score())->getScoreByExamID($paper->exam_id, session('studentId'));
         $ept = time() + $offset;
-        $score->expect_time = $ept;
+        if (!$score->expect_time) {
+            $score->expect_time = $ept;
+        }
         $score->save();
-        return $ept;
+        return $score->expect_time;
+    }
+
+    public function insert() {
+        $post = Request::instance()->post();
+
+        $Paper = new Paper();
+
+        $Paper->name = $post['name'];
+        $Paper->sex = $post['sex'];
+        $Paper->work_number = $post['work_number'];
+        $Paper->password = $post['password'];
+        $Paper->room_id = $post['room_id'];
+
+        if ($Paper->save()) {
+            return $this->success('保存成功！', 'index');
+        } else {
+            return $this->error('保存失败！');
+        }
     }
 }
